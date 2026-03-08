76edf01316d84748e380ff20c7e1f956e6e7e265
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/arrangements/page.tsx
+++ app/arrangements/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ArrangementCard from '../components/ArrangementCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Arrangements | Florist',
